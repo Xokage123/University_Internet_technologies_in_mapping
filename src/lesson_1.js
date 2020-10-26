3071c85d6c2390a76a39567862e9d679ab22a8d9
Maks <59100893+Xokage123@users.noreply.github.com>
2020-10-26 15:11:16 +0300

    Пара 26.10.2020

--- src/lesson_1.js
+++ src/lesson_1.js
@@ -1,38 +1,68 @@
 "use stcrict";
 
 // Инициализация карты
-function init() {
-    const myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 11,
-    }, {
-        searchControlProvider: 'yandex#search',
-    });
+// function init() {
+//     const myMap = new ymaps.Map("map", {
+//         center: [55.76, 37.64],
+//         zoom: 11,
+//     }, {
+//         searchControlProvider: 'yandex#search',
+//     });
+// }
+// ymaps.ready(init);
+
+//Лефлет подключение
+
+const mymap = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 0,
+    maxZoom: 19
+}).addTo(mymap);
+
+// Создание события щелчка по карте
+const popup = L.popup();
+
+function onMapClick(e) {
+    console.log(e);
+    console.log(e.latlng);
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Координаты куда вы кликнули " + e.latlng.toString())
+        .openOn(mymap);
 }
-ymaps.ready(init);
+
+const myMarker = L.marker([0, 0]);
+
+mymap.on('click', onMapClick);
 // Работа с автозаполнением
 $(() => {
     const availableTags = [
-        { label: "Сочи", value: this.label },
-        { label: "Москва", value: this.label },
-        { label: "Дмитров", value: this.label },
-        { label: "Екатеренбруг", value: this.label },
-        { label: "Рязань", value: this.label },
-        { label: "Орёл", value: this.label },
-        { label: "Иваново", value: this.label },
-        { label: "Коломна", value: this.label },
-        { label: "Анапа", value: this.label },
-        { label: "Тамбов", value: this.label },
-        { label: "Самара", value: this.label },
-        { label: "Саратов", value: this.label },
-        { label: "Липецк", value: this.label },
-        { label: "Пенза", value: this.label },
-        { label: "Краснодар", value: this.label },
+        { label: "Сочи", value: this.label, coord: { lat: 43.582579, lng: 39.722246 } },
+        { label: "Москва", value: this.label, coord: { lat: 55.752004, lng: 37.622774 } },
+        { label: "Дмитров", value: this.label, coord: { lat: 56.344516, lng: 37.519808 } },
+        { label: "Екатеренбруг", value: this.label, coord: { lat: 56.833949, lng: 60.619748 } },
+        { label: "Рязань", value: this.label, coord: { lat: 54.622978, lng: 39.737421 } },
+        { label: "Орёл", value: this.label, coord: { lat: 52.955588, lng: 36.066987 } },
+        { label: "Иваново", value: this.label, coord: { lat: 56.998418, lng: 40.97201 } },
+        { label: "Коломна", value: this.label, coord: { lat: 55.087171, lng: 38.770441 } },
+        { label: "Анапа", value: this.label, coord: { lat: 44.893434, lng: 37.314993 } },
+        { label: "Тамбов", value: this.label, coord: { lat: 52.703352, lng: 41.451951 } },
+        { label: "Самара", value: this.label, coord: { lat: 53.193529, lng: 50.156867 } },
+        { label: "Саратов", value: this.label, coord: { lat: 51.525834, lng: 45.982168 } },
+        { label: "Липецк", value: this.label, coord: { lat: 51.646317, lng: 39.201803 } },
+        { label: "Пенза", value: this.label, coord: { lat: 53.209158, lng: 45.004057 } },
+        { label: "Краснодар", value: this.label, coord: { lat: 45.032386, lng: 38.979773 } },
     ];
     $("#tags").autocomplete({
         source: availableTags,
         select: function(event, ui) {
-            alert(`Вы выбрали город: ${ui.item.label}`);
+            myMarker
+                .setLatLng([ui.item.coord.lat, ui.item.coord.lng])
+                .bindPopup(`<h2>Этот город ${ui.item.label}</h2>`)
+                .openPopup()
+                .addTo(mymap);
+            mymap.flyTo([ui.item.coord.lat, ui.item.coord.lng], 12)
         },
         minLength: 1,
     });
@@ -50,11 +80,9 @@ $(".menu_button-3").click(function(ev) {
 })
 
 function countingRabbits(ev) {
-    countR += 1;
-    alert(`${countR} кролик`);
+    mymap.fitWorld();
 }
 
 function countingChicken(ev) {
-    countChicken += 1;
-    alert(`${countChicken} курица`);
+    mymap.flyTo([55.756136, 37.63593], 18);
 }
